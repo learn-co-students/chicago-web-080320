8b74a42fca546bddf1f4a572f9fd16b834b6ed32
ihollander <ianholla@gmail.com>
2020-08-19 10:25:27 -0400

    before hashes and the internet

---
+++ 10-hashes-and-the-internet/README.md
@@ -0,0 +1,18 @@
+# Hashes and the Internet
+
+### Define
+
+* Request / Response
+* Client / Server
+* HTTP Verbs, Status Codes, Response Body
+* Developer console
+* APIs
+* JSON
+
+### Deliverables
+
+* Write an application that takes a search term from a user
+* Make a Request to the GoogleBooks API and get back some results
+  * Docs: https://developers.google.com/books/docs/v1/using#PerformingSearch
+  * https://www.googleapis.com/books/v1/volumes?q=ruby+programming
+* Display the titles, author names joined by an &, and first 100 characters of the description for each book
---
+++ 10-hashes-and-the-internet/app.rb
@@ -0,0 +1,14 @@
+# 'https://www.googleapis.com/books/v1/volumes?q=ruby+programming'
+
+# require 'rest-client'
+# require 'json'
+# require 'pry'
+
+# * Write an application that takes a search term from a user
+# * Write an application that takes a search term from a user
+# * Make a Request to the GoogleBooks API and get back some results
+#   * Docs: https://developers.google.com/books/docs/v1/using#PerformingSearch
+#   * https://www.googleapis.com/books/v1/volumes?q=ruby+programming
+# * Display the titles, author names joined by an &, and first 100 characters of the description for each book
+
+puts "Welcome to our Book Searcher!"

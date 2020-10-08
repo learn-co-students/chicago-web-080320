85e1a181a0248eb5fc8c32284b49e1c2c288c2b7
Raza Jafri <sjafri@SF-US04304-MBP13.local>
2020-10-07 19:47:08 -0500

    asf

---
+++ petslab/src/components/Pet.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+class Pet extends React.Component {
+  render() {
+    const { name, isAdopted, type, gender, id, weight, age } = this.props.pet
+    
+    return (
+      <div className="card">
+        <div className="content">
+          <a className="header">
+            { gender === 'female' ? '♀' : '♂' }
+            { name }
+          </a>
+          <div className="meta">
+            <span className="date">{type}</span>
+          </div>
+          <div className="description">
+            <p>Age: {age}</p>
+            <p>Weight: {weight}</p>
+          </div>
+        </div>
+        <div className="extra content">
+      {
+        isAdopted ? 
+        <button className="ui disabled button">Already adopted</button>
+        :
+        <button className="ui primary button" onClick={() => this.props.onAdoptPet(id)}>Adopt pet</button>
+      }
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Pet
---
+++ petslab/src/components/PetBrowser.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Pet from './Pet'
+
+class PetBrowser extends React.Component {
+  renderPets = () => {
+    return this.props.pets.map(petObj => {
+      return <Pet pet={petObj} onAdoptPet={this.props.onAdoptPet} />
+    })
+  }
+
+  render() {
+    return <div className="ui cards">
+      { this.renderPets() }
+      </div>
+  }
+}
+
+export default PetBrowser
